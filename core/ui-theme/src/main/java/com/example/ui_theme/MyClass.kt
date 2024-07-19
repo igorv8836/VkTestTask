016b4d2899db165735e6661d99d8f7d97b6775df
igorv8836 <igorv8836@mail.ru>
2024-07-19 14:20:58 +0300

    init commit, creating a module structure

---
+++ core/ui-theme/src/main/java/com/example/ui_theme/MyClass.kt
@@ -0,0 +1,4 @@
+package com.example.ui_theme
+
+class MyClass {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "VkTestTask"
+include(":app")
+include(":core:common")
+include(":core:network")
+include(":core:database")
+include(":feature:currency_converter")
+include(":core:ui-theme")
